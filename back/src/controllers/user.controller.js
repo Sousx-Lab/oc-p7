69c14b9bdde4ae6b388b485e94acdf1ee0309810
Nassim Hattab <nssmhttb@gmail.com>
2022-02-11 23:03:36 +0100

    feat: login

--- back/src/controllers/user.controller.js
+++ back/src/controllers/user.controller.js
@@ -1,13 +1,14 @@
-const { User } = require('../models/index');
+const { User, Token } = require('../models/index');
 const {jsonErrors} = require('../middlewares/http/http.json.errors');
 const bcrypt = require('bcrypt');
 const jwt = require('../middlewares/security/jwt');
+const crypto = require('crypto');
 
 /**SignUp */
 exports.signup = async(req, res, next) => {
     try {
         const user = await User.build({...req.body, roles: []});
-        user.validate()
+        await user.validate()
         user.password = await bcrypt.hash(user.password, 10);
         await user.save();
         return res.http.Created({message: "User was created succesfuly"});
@@ -19,22 +20,36 @@ exports.signup = async(req, res, next) => {
 /**Login */
 exports.login = async(req, res, next) => {
     
-    // console.log(req.signedCookies['auth'])
+    // console.log(req.signedCookies['access_token'])
     try {
         const user = await User.findOne({ where: {email: req.body.email}})
         if(!user){
-            return res.http.NotFound({error: {message: 'Email or password invalid'}});
+            return res.http.BadRequest({error: {message: 'Email or password invalid'}});
         }
         if(!await bcrypt.compare(req.body.password, user.password)){
-            return res.http.NotFound({error: {message: 'Email or password invalid'}});
+            return res.http.BadRequest({error: {message: 'Email or password invalid'}});
         }
+        const refreshToken = crypto.randomBytes(128).toString('base64');
+        const xsrfToken = crypto.randomBytes(64).toString('hex');
+        
         const cookieOptions = {
-            maxAge: 1000 * 60 * 15, 
-            httpOnly: true, 
-            signed: true
+            maxAge: parseInt(process.env.COOKIE_EXPIRES_AT, 10),
+            httpOnly: true,
+            signed: true,
+            path: '/'
         }
-        res.cookie("auth", jwt.jwtSign(user), cookieOptions)
-        return res.status(200).json({auth: "123456789"})
+       
+        await Token.create({
+            token: refreshToken,
+            user_id: user.id, 
+            expires_at: Date.now() + parseInt(process.env.JWT_TOKEN_EXPIRES_IN,10)
+        });
+
+        res.cookie("access_token", jwt.jwtSign(user, xsrfToken), cookieOptions);
+        res.cookie("refresh_token", refreshToken, {...cookieOptions, path: '/token'});
+        
+        return res.http.Ok({xsrfToken: xsrfToken, });
+
     } catch (error) {
         return jsonErrors(error, res)
     }
