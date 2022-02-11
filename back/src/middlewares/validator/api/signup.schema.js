492f30076ef08a9913ede8e50c301e280e9ed1dd
Nassim Hattab <nssmhttb@gmail.com>
2022-02-11 22:56:48 +0100

    refactor: add max length user firstname & lastname

--- back/src/middlewares/validator/api/signup.schema.js
+++ back/src/middlewares/validator/api/signup.schema.js
@@ -20,21 +20,26 @@ exports.signupSchema =
           }),
         firstname: Joi
           .string()
+          .max(64)
           .custom(isHtmlTag, 'string.html')
           .required()
           .messages({
             'string.base': "firstname must be a string",
             'any.required': "firstname is required",
             'string.empty': "firstname must not be empty",
+            'string.max': "firstname must have max 64 characters",
+            'string.alphanum': "must only contain alpha-numeric characters"
           }),
         lastname: Joi
           .string()
+          .max(64)
           .custom(isHtmlTag, 'string.html')
           .required()
           .messages({
             'string.base': "lastname must be a string",
             'any.required': "lastname is required",
-            'string.empty': "lasstname must not be empty",
+            'string.empty': "lastname must not be empty",
+            'string.max': "lastname must have max 64 characters"
           }),
         password: Joi
           .string()
