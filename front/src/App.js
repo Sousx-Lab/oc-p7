614c8d8c1e9fd0f13abbb8510b8a2bad875f8915
Nassim Hattab <nssmhttb@gmail.com>
2022-03-14 14:32:05 +0100

    feat: full screen loader

--- front/src/App.js
+++ front/src/App.js
@@ -9,51 +9,48 @@ import { UserContext } from "./contexts/UserContext";
 import { ToastContainer } from 'react-toastify';
 import Header from "./components/Header";
 import AuthSideScreen from './components/AuthSideScreen';
+import FullScreenLoader from './components/FullScreenLoader';
 import 'react-toastify/dist/ReactToastify.css';
 import './assets/scss/app.scss';
 import { loadUser } from "./hooks/loadUser";
 
-
 function App(){
-    
-    // const [user, setUser, isLoading] = loadUser();
-    const [user, setUser] = useState(true)
-    // if(isLoading){
-    //   return (
-    //     <div className="row text-center justify-content-center h-100">
-    //       <div className="col-6 align-self-center">
-    //         <div className="spinner-border text-primary" style={{width: 3+'rem', height: 3+'rem'}} role="status"></div>
-    //         <div className="sr-only"><strong>Loading...</strong></div>
-    //         </div>
-    //     </div>
-    //   )
-    // }
+
+    // const [user, setUser] = useState(true)
+    // const isLoading = true
+
+    const [user, setUser, isLoading] = loadUser();
+    if(isLoading){
+      return (
+        <FullScreenLoader />
+      )
+    }
     return (
     <UserContext.Provider value={{ user, setUser }}>
       <BrowserRouter >
         <Routes>
-        <Route element={<AuthSideScreen />} >
+          <Route element={<AuthSideScreen />} >
             <Route path="/login" element={<LoginPage />}/>
             <Route path="/signup" element={<SignUpPage />} />
-        </Route>
-            <Route element={<ProtectedRoutes />} >
-              <Route element={<Header />} >
-                <Route index path="/" element={<HomePage/>} />
-              </Route>
+          </Route>
+          <Route element={<ProtectedRoutes />} >
+            <Route element={<Header />} >
+              <Route index path="/" element={<HomePage/>} />
             </Route>
-          </Routes>
-      <ToastContainer 
-        position="bottom-left"
-          autoClose={5000}
-            hideProgressBar={false}
-              newestOnTop
-                closeOnClick
-                  rtl={false}
-                pauseOnVisibilityChange
-              draggable
-            pauseOnHover
-          />
-        </BrowserRouter>
+          </Route>
+        </Routes>
+        <ToastContainer 
+          position="bottom-left"
+            autoClose={5000}
+              hideProgressBar={false}
+                newestOnTop
+                  closeOnClick
+                    rtl={false}
+                  pauseOnVisibilityChange
+                draggable
+              pauseOnHover
+            />
+      </BrowserRouter>
     </UserContext.Provider>
     )
 }
---
+++ front/src/components/FullScreenLoader.jsx
@@ -0,0 +1,13 @@
+
+const FullScreenLoader = () => {
+    return (
+        <div className="row text-center justify-content-center h-100">
+          <div className="col-6 align-self-center">
+            <div className="spinner-border text-primary mb-3" style={{width: 3+'rem', height: 3+'rem'}} role="status"></div>
+            <div className="sr-only lead"><strong>Loading...</strong></div>
+            </div>
+        </div>
+      )
+}
+
+export default FullScreenLoader;
