4b0b11dd4957509c9035f3566edf7c7c13ee4232
Nassim Hattab <nssmhttb@gmail.com>
2022-03-19 12:26:00 +0100

    refactor: defer bootstrap js & auth image

--- front/src/components/AuthSideScreen.jsx
+++ front/src/components/AuthSideScreen.jsx
@@ -5,8 +5,8 @@ const AuthSideScreen = () => {
     return(
         <div className='container-fluid vh-100 p-0'>
             <div className='d-flex col-12 m-0'>
-                <div className='d-none d-lg-block col-lg-7 p-0 '>
-                <img className='vh-100' src="https://source.unsplash.com/random/1650×1080/?nature" width="100%" height="auto" alt="Groupomania" />
+                <div className='d-none d-lg-block col-lg-7 p-0 h-100'>
+                <img className='vh-100' src="https://source.unsplash.com/random/1650×1080/?nature" async width="100%" height="auto" alt="Groupomania" />
             </div>
             <Outlet />
         </div>
