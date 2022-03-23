38044e3a70ed14e742a54049793f3522e197ee74
Nassim Hattab <nssmhttb@gmail.com>
2022-03-23 16:24:09 +0100

    translation: modal close button

--- front/src/components/CommentModal.jsx
+++ front/src/components/CommentModal.jsx
@@ -47,7 +47,7 @@ const CommentModal = ({ post }) => {
                         <Editor />
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
                     </div>
                 </div>
             </div>
