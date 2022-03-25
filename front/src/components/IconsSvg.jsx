c8dbbb11076352114f200859fdfd90a61b0058dc
Nassim Hattab <nssmhttb@gmail.com>
2022-03-25 11:27:49 +0100

    fix: svg properties

--- front/src/components/IconsSvg.jsx
+++ front/src/components/IconsSvg.jsx
@@ -23,9 +23,9 @@ export const UploadMedia = ({ size = 22, stroke = "#6c6a6a", fill = "none", stro
     <path d="m20.4,14.5l-4.4,-4.5l-12,10" />
 </svg>);
 
-export const MoreVertical = ({size = 24, stroke = "#6c6a6a"}) => 
+export const MoreVertical = ({size = 24, stroke = "#6c6a6a", strokeWidth = "1.5"}) => 
 (<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" 
-    fill="none" stroke={stroke} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+    fill="none" stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
     <path d="M11,12a1,1 0 1,0 2,0a1,1 0 1,0 -2,0"/>
     <path d="M18,12a1,1 0 1,0 2,0a1,1 0 1,0 -2,0"/>
     <path d="M4,12a1,1 0 1,0 2,0a1,1 0 1,0 -2,0" />
